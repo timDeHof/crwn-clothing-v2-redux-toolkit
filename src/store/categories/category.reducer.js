c70bf962c9392dcbb15921d3264f18da3573abe0
timDeHof <this.is.tim.dehof@gmail.com>
2023-02-17 16:17:04 -0500

    refactor: refactor categories reducer into a categories slice

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,19 +1,20 @@
-import { CATEGORIES_ACTION_TYPES } from './category.types';
+// import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { createSlice } from "@reduxjs/toolkit"
 
 export const CATEGORIES_INITIAL_STATE = {
   categories: [],
-};
+}
+export const categoriesSlice = createSlice({
+  name: "categories",
+  initialState: CATEGORIES_INITIAL_STATE,
+  reducers: {
+    setCategories(state, action) {
+      state.categories = action.payload
+    },
+  },
+})
 
-export const categoriesReducer = (
-  state = CATEGORIES_INITIAL_STATE,
-  action = {}
-) => {
-  const { type, payload } = action;
+export const { setCategories } = categoriesSlice.actions
 
-  switch (type) {
-    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-      return { ...state, categories: payload };
-    default:
-      return state;
-  }
-};
+export const categoriesReducer = categoriesSlice.reducer;
+
