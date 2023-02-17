0a48e55dd5a8156df09f6cf9b507ec75cb965abc
timDeHof <this.is.tim.dehof@gmail.com>
2023-02-17 16:57:36 -0500

    refactor: change cart reducer into a cartSlice

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,11 +1,6 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from "react-redux"
 
-import {
-  clearItemFromCart,
-  addItemToCart,
-  removeItemFromCart,
-} from '../../store/cart/cart.action';
-import { selectCartItems } from '../../store/cart/cart.selector';
+import { clearItemFromCart, addItemToCart, removeItemFromCart } from "../../store/cart/cart.reducer"
 
 import {
   CheckoutItemContainer,
@@ -15,18 +10,15 @@ import {
   Arrow,
   Value,
   RemoveButton,
-} from './checkout-item.styles';
+} from "./checkout-item.styles"
 
 const CheckoutItem = ({ cartItem }) => {
-  const { name, imageUrl, price, quantity } = cartItem;
-  const dispatch = useDispatch();
-  const cartItems = useSelector(selectCartItems);
+  const { name, imageUrl, price, quantity } = cartItem
+  const dispatch = useDispatch()
 
-  const clearItemHandler = () =>
-    dispatch(clearItemFromCart(cartItems, cartItem));
-  const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
-  const removeItemHandler = () =>
-    dispatch(removeItemFromCart(cartItems, cartItem));
+  const clearItemHandler = () => dispatch(clearItemFromCart(cartItem))
+  const addItemHandler = () => dispatch(addItemToCart(cartItem))
+  const removeItemHandler = () => dispatch(removeItemFromCart(cartItem))
 
   return (
     <CheckoutItemContainer>
@@ -42,7 +34,7 @@ const CheckoutItem = ({ cartItem }) => {
       <BaseSpan> {price}</BaseSpan>
       <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
     </CheckoutItemContainer>
-  );
-};
+  )
+}
 
-export default CheckoutItem;
+export default CheckoutItem
